f3843f301ce1af4933e43a45b22c86d6538eddb0
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-19 23:00:30 +0800

    Separated the creation of new anecdotes into its own component called AnecdoteForm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import anecdoteReducer from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -10,13 +11,6 @@ const App = () => {
 		dispatch(anecdoteReducer.increaseVote(id))
     console.log('vote', id)
   }
-
-	const addAnecdote = (event) => {
-		event.preventDefault()
-		const content = event.target.anecdote.value
-		event.target.anecdote.value = ''
-		dispatch(anecdoteReducer.newAnecdote(content))
-	}
 
   return (
     <div>
@@ -33,10 +27,7 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name='anecdote'/></div>
-        <button type='submit'>create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
